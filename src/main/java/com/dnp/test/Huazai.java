2e050e4859f8d58ef4e5b2e87c28b0bbe1222a78
huazai <379924269@qq.com>
2020-04-09 16:10:59 +0800

    1、添加了几个常用的工具类来测试 2、添加了jar maven打包工具，参考地址：https://blog.csdn.net/xiao__gui/article/details/47341385

---
+++ src/main/java/com/dnp/test/Huazai.java
@@ -0,0 +1,12 @@
+package com.dnp.test;
+
+/**
+ * @author huazai
+ * @description
+ * @date 2020/4/9
+ */
+public class Huazai {
+    public static void main(String[] args) {
+        System.out.println("hello word!");
+    }
+}
---
+++ src/main/java/com/dnp/test/io/IoTest.java
@@ -0,0 +1,11 @@
+package com.dnp.test.io;
+
+/**
+ * @author huazai
+ * @description I/O实用程序集合
+ * @date 2020/4/9
+ */
+public class IoTest {
+
+
+}
